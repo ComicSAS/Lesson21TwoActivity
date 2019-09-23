6978c9897fa6a3e3fa74d1b7bee08c5a7b8eeba7
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-09-23 21:21:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson21twoactivity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lesson21twoactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        if (v.getId() == R.id.btnActTwo)
+            startActivity(intent);
+
+    }
+}
+
